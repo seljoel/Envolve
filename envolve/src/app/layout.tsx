229bdc296a3cbda34b478d63df1ef0b07c809b8a
aadithjkrishna <aadithjkrishnan45@gmail.com>
2025-07-26 22:42:25 +0530

    Changed stuff

--- envolve/src/app/layout.tsx
+++ envolve/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ThemeProvider from "@/components/ThemeProvider";
 import Link from "next/link";
-import "./globals.css";
+import "./styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
